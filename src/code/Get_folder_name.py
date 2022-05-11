a39f3772d307f30142b474674d76b12d446de3f6
KeithLin7024 <keithlin724@gmail.com>
2022-05-11 15:43:32 +0800

    first commit

---
+++ src/code/Get_folder_name.py
@@ -0,0 +1,57 @@
+from rich import print as rprint
+import os
+
+folderTree = ['CW', 'HT']
+dataFolder = 'src\data'
+
+
+folderFileName = dict()
+for nameFolder in folderTree:
+    name = os.path.join(os.path.abspath(dataFolder), nameFolder)
+
+    try:
+        folderFileName.update({nameFolder: os.listdir(name)})
+
+    except Exception as e:
+        print(e)
+        exit()
+
+rprint(folderFileName)
+
+# make a folder name
+absFolderPath = os.path.join(os.path.abspath('src'), 'name of folder')
+if os.path.exists(absFolderPath) == False:
+    os.mkdir(absFolderPath)
+
+for key, listOfFileName in folderFileName.items():
+
+    fileNameTmp = os.path.join(absFolderPath, ''.join([key, 'Folder', '.txt']))
+
+    with open(fileNameTmp, mode='w') as f:
+        for i in listOfFileName:
+            f.write(i+'\n')
+
+# make a abs file path in file
+PathOfFolderPath = os.path.join(os.path.abspath('src'), 'name absfolder')
+if os.path.exists(PathOfFolderPath) == False:
+    os.mkdir(PathOfFolderPath)
+
+
+listSaveAbsPath = dict()
+for nameFolder in folderTree:
+    for root, directories, files in os.walk(os.path.join(os.path.abspath(dataFolder), nameFolder)):
+        tmp = [os.path.join(root, name) for name in files]
+        listSaveAbsPath.update({nameFolder: tmp})
+
+        '''
+        for name in directories:
+            print(os.path.join(root, name))'''
+rprint(listSaveAbsPath)
+for key, listOfFilePath in listSaveAbsPath.items():
+
+    fileNameTmp = os.path.join(PathOfFolderPath, ''.join(
+        [key, 'Folder abs Path', '.txt']))
+
+    with open(fileNameTmp, mode='w') as f:
+        for i in listOfFilePath:
+            f.write(i+'\n')
---
+++ src/ref/ref.md
@@ -0,0 +1,3 @@
+# ref 
+
+https://careerkarma.com/blog/python-list-files-in-directory/#:~:text=The%20Python%20os%20library%20is%20used%20to%20list,of%20every%20file%20in%20an%20entire%20file%20tree.
