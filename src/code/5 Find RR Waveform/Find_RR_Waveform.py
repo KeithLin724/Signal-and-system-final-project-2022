1553c0871320b81705ab2fc2f9c4746317647dbf
KeithLin7024 <keithlin724@gmail.com>
2022-05-29 02:26:44 +0800

    add comment

--- src/code/5 Find RR Waveform/Find_RR_Waveform.py
+++ src/code/5 Find RR Waveform/Find_RR_Waveform.py
@@ -15,7 +15,7 @@ dpi = get_ppi()
 
 def to_rr_interval(dataHR: list) -> list:
     """_summary_
-
+        let the HR data to be a RR interval
     Args:
         dataHR (list):input the HR list data 
 
@@ -26,9 +26,11 @@ def to_rr_interval(dataHR: list) -> list:
     return [60/float(i) for i in dataHR]
 
 
+# about the path
 mainFolder, branchFolder = 'src', 'RR Data'
 saveMainFolder = os.path.join(mainFolder, branchFolder)
 saveRrCSVFolder = os.path.join(mainFolder, 'RR_csv')
+
 # save folder
 if not os.path.exists(saveMainFolder):
     os.mkdir(saveMainFolder)
@@ -40,37 +42,41 @@ taData = (dataBase[dataName[0]]['HR'], dataBase[dataName[1]]['HR'])
 taDataDict = dict(zip(dataName, taData))
 dataStruct = []
 
+# make the dataChange for save the tuple data (file state , DataFrame and RR data)
 dataChangeDict = {dataName[0]: deepcopy(dataStruct),
                   dataName[1]: deepcopy(dataStruct)}
 
 for taName, valDataList in taDataDict.items():
     for fileObj in valDataList:
+        # get data from the file
         tmpFileList = fileObj.get_file_data()
-        tmpFileList = sorted(tmpFileList)
+        tmpFileList = sorted(tmpFileList)  # sort data
         _, fileState, _ = fileObj.get_file_type_detail()
         tranToRR = to_rr_interval(tmpFileList)
 
-        # print(tranToRR)
-        #print(len(tmpFileList) == len(tranToRR))
         tmpDf = pd.DataFrame({'Heart Beats': tmpFileList,
                               'RRI Value': tranToRR})
-        RRSerial = pd.Series(tranToRR)
+        RRSeries = pd.Series(tranToRR)
 
-        dataChangeDict[taName].append((fileState, tmpDf, RRSerial))
+        # save data
+        dataChangeDict[taName].append((fileState,   # the state of the file
+                                       tmpDf,       # Data Frame of the RR and Heart Beats
+                                       RRSeries))   # RR Series (for other code )
 
 
 # check the data
 print(dataChangeDict)
+
 # to file
-
 for taName, valTuples in dataChangeDict.items():
-    savePath = os.path.join(saveMainFolder, taName)
 
+    # open save file path
+    savePath = os.path.join(saveMainFolder, taName)
     if not os.path.exists(savePath):
         os.mkdir(savePath)
 
-    rrCSVPath = os.path.join(saveRrCSVFolder, taName)  # save for rr csv file
-
+    # save for rr csv file
+    rrCSVPath = os.path.join(saveRrCSVFolder, taName)
     if not os.path.exists(rrCSVPath):
         os.mkdir(rrCSVPath)
 
@@ -80,13 +86,15 @@ for taName, valTuples in dataChangeDict.items():
         if not os.path.exists(saveBranchFolder):
             os.mkdir(saveBranchFolder)
 
-        title = f'RR-interval {state}'
+        title = f'RR-interval {state}'  # about the file name
 
+        # Data Frame to png
         save_to_png(folderPath=saveBranchFolder,
                     titleStr=title,
                     x_ticks=True,
                     data=dataDf,
                     dpi=dpi)
+
         # save file name
         fileCSVSavePath = os.path.join(saveBranchFolder, f'{title}.csv')
         dataDf.to_csv(fileCSVSavePath, index=False)
